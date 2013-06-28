3bfde3e4d3b6d93c520ff9c7834ac2634919d012
microwish <microwish@gmail.com>
2013-06-28 13:10:59 +0800

    Update mysql_test.lua

--- mysql_test.lua
+++ mysql_test.lua
@@ -6,35 +6,43 @@ local mysql = require("mysql")
 --mysql.OPTION.INIT_COMMAND = "aaa"
 
 for k, v in pairs(mysql.OPTION) do
-  print(k, v)
+	print(k, v)
 end
 --]]
 
 local dbconfig = {
-	host = "10.10.10.10",
+	host = "10.1.1.1",
 	port = 3606,
 	user = "user",
 	passwd = "passwd",
-	db = "dbname"
+	--db = "dbname"
+	db = "dbname2"
 }
 
 local options = {}
 options[mysql.OPTION.CONNECT_TIMEOUT] = 3
 options[mysql.OPTION.CHARSET_NAME] = "utf8"
 
-local dbh = mysql.new(dbconfig.host, dbconfig.user, dbconfig.passwd, dbconfig.db, dbconfig.port, options)
+local dbh = assert(mysql.new(dbconfig.host, dbconfig.user, dbconfig.passwd, dbconfig.db, dbconfig.port, options))
 
-dbh:set_charset("utf8")
+assert(dbh:set_charset("utf8"))
 
-local l, sql = dbh:real_escape_string("SELECT * FROM `developer` LIMIT 4")
+assert(dbh:select_db("dbname"))
+
+local l, sql = assert(dbh:real_escape_string("SELECT * FROM `tablename` LIMIT 4"))
 print(l, sql)
 
-local r1, r2 = dbh:query(sql)
+local r1, r2 = assert(dbh:query(sql))
 print(r1, #r2)
+--[[
 for i, t in ipairs(r2) do
 	for k, v in pairs(t) do
 		print(k, v)
 	end
 end
+--]]
 
-dbh:close()
+--dbh:close()
+mysql.delete(dbh)
+
+--assert(dbh:set_charset("utf8"))
